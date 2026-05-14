a551b7c849c9f4101014bd8ff915b51134b26aae
TeunLammerts <teun.lammerts.2004@gmail.com>
2026-05-14 11:18:14 +0200

    add proof that subgraphs are n-listcolorable

--- LeanPlanarGraphs/ListColoring.lean
+++ LeanPlanarGraphs/ListColoring.lean
@@ -3,7 +3,7 @@ import Mathlib
 
 namespace SimpleGraph
 
-variable {V : Type*} (G : SimpleGraph V)
+variable {V : Type*} (G H : SimpleGraph V)
 
 /-- an `α`-List coloring of a simple graph is an `α`-coloring with
 the added condition that the colors for each vertex are restricted to
@@ -15,30 +15,29 @@ structure ListColoring (α : Type*) (f : V → Finset α) where
 
 variable {α : Type*} {f : V → Finset α} (LC : G.ListColoring α f)
 
-/- extends Coloring.valid to ListColoring
+/-- extends Coloring.valid to ListColoring
 -/
 theorem ListColoring.colorValid {v w : V} (h : G.Adj v w) : LC.coloring v ≠ LC.coloring w := by
   exact LC.coloring.valid h
 
-
-/- proof that the colors in the ListColoring are in the lists
+/-- proof that the colors in the ListColoring are in the lists
 -/
 theorem ListColoring.listValid {v : V} : LC.coloring v ∈ f v := by
   exact LC.color_mem_f v
 
-/- a KList is defined as a function f : V → Finset α where f v has cardinality k
+/-- a KList is defined as a function f : V → Finset α where f v has cardinality k
   for all v ∈ V
 -/
 structure KList {V : Type*} (α : Type*) (k : ℕ) where
   f : V → Finset α
   cardinality_K : ∀(v : V), (f v).card = k
 
-/- a SimpleGraph G is k list-colorable if for every list assinging k colors
+/-- a SimpleGraph G is k list-colorable if for every list assinging k colors
 to every vertex there exists an `ℕ`list-coloring
 -/
 def ListColorable (k : ℕ) : Prop := ∀(list : KList ℕ k), Nonempty (G.ListColoring ℕ list.f)
 
-/- the KList which assigns to every vertex the set Finset.range k
+/-- the KList which assigns to every vertex the set Finset.range k
 -/
 def range_Klist (k : ℕ) : @KList V ℕ k where
   f := fun v ↦ Finset.range k
@@ -46,10 +45,9 @@ def range_Klist (k : ℕ) : @KList V ℕ k where
     intro v
     apply Finset.card_range
 
-/- a proof of the fact that if G is n-ListColorable that G is also n-Colorable for some  n > 0
+/-- a proof of the fact that if G is n-ListColorable, G is also n-Colorable
 -/
-theorem ListColoring.ListColorable_imp_colorable_pos_n (n : ℕ) [NeZero n]
-        : G.ListColorable n → G.Colorable n := by
+theorem ListColoring.ListColorable_imp_colorable (n : ℕ) : G.ListColorable n → G.Colorable n := by
   intro ListColorable
   let fₙ := @range_Klist V n
 
@@ -69,3 +67,30 @@ theorem ListColoring.ListColorable_imp_colorable_pos_n (n : ℕ) [NeZero n]
   exact Finset.mem_range.mp this
 
 end SimpleGraph
+
+/-- given a coloring of a graph create a coloring of a subgraph
+-/
+def Coloring.subcoloring {H : SimpleGraph V} (h : H ≤ G) (hc : G.Coloring α) : H.Coloring α := by
+  exact hc.comp (.ofLE h)
+
+/-- proof that if G is n-ListColorable, then so is any subgraph
+-/
+theorem ListColorable.mono_left {H : SimpleGraph V} (n : ℕ) (sub : H ≤ G)
+  : G.ListColorable n → H.ListColorable n := by
+  intro GnListColorable list
+
+  have existsGncoloring : Nonempty (G.ListColoring ℕ list.f) :=
+    GnListColorable list
+
+  let GnListColoring := existsGncoloring.some
+
+  let HnListColoring : H.ListColoring ℕ list.f :=
+    {coloring := Coloring.subcoloring sub GnListColoring.coloring, color_mem_f := by
+      intro v
+      rw[Coloring.subcoloring]
+      simp
+      exact GnListColoring.color_mem_f v
+    }
+
+  apply Nonempty.intro HnListColoring
+
